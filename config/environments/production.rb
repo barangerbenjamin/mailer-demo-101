557c38d48d9c004d7c4962396389fbe59ea0300c
Benjamin Baranger <baranger.benjamin@gmail.com>
2018-05-29 10:04:23 +0100

    change lidevery to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  # config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
@@ -65,7 +65,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mailer-demo-101_#{Rails.env}"
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "mailer-demo-101.herokuapp.com" }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
